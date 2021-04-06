8bd007bef02edb1323ae16e9b1dd4f09141ee177
Thanh Vo <tvo@datasolutiontech.com>
2021-04-06 03:23:51 -0400

    tvo react simple grid

---
+++ src/components/TVoGridView.tsx
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import {IColumnDefs,TVoSimpleGrid} from 'tvo-react-simple-grid'
+import 'tvo-react-simple-grid/dist/index.css'
+import { ColumnDefinition } from '../data/ColumnDefinition';
+import { PersonalInfo } from '../data/PersonalInfo';
+
+export interface ITVoGridViewProps {
+}
+
+export interface ITVoGridViewState {
+  dataSource: any[];
+  columns: IColumnDefs;
+  startRow: number;
+  rowPerPage:number;
+  dataSourceName: string;
+}
+
+export default class TVoGridView extends React.Component<ITVoGridViewProps, ITVoGridViewState> {
+  private _dataSource = PersonalInfo;
+  private isLoading: boolean =false;
+  constructor(props: ITVoGridViewProps) {
+    super(props);
+
+    this.state = {
+      dataSource: [],
+      columns: ColumnDefinition,
+      startRow: 0,
+      rowPerPage:25,
+      dataSourceName: ColumnDefinition.dataSourceName
+    }
+  }
+
+  private getData(){
+    let personalInfo: any[] = [];
+    var endRow = this.state.startRow + this.state.rowPerPage;
+    for(var i=this.state.startRow;i < endRow;i++){
+        personalInfo.push(this._dataSource[i]);
+    }
+    var newStartRow = this.state.startRow + this.state.rowPerPage;
+    this.setState({dataSource:[...this.state.dataSource,...personalInfo],startRow: newStartRow});  
+}
+
+  public componentDidMount(){
+      this.getData();
+  }
+  
+  public componentDidUpdate(prevProps:ITVoGridViewProps,prevState: ITVoGridViewState){
+      if(this.state.dataSource.length > prevState.dataSource.length )
+      {
+          this.isLoading = false; //set loading to false after the component rendered
+      }
+  }
+  
+  private _onScroll = (dataSourceName: string) => {
+     if(!this.isLoading)
+     this.getData();
+  };
+
+  private _onRowItemRender = (
+    dataSourceName: string,
+    columnKey: string,
+    fieldName: string,
+    item: any
+  ) => {
+    if (dataSourceName === this.state.dataSourceName)
+         return <span>{item[fieldName]}</span>;
+  };
+
+  private _onRowItemClick = (
+    dataSourceName: string,
+    columnKey: string,
+    fieldName: string,
+    value: string,
+    item: any
+  ) => {
+    console.log(value);
+  };
+
+  private _onHeaderItemRender = (
+    dataSourceName: string,
+    columnKey: string,
+    fieldName: string,
+    headerName: string
+  ) => {
+    if (dataSourceName === this.state.dataSourceName ) {
+      return <span>{headerName}</span>;
+    }
+  };
+
+  private _onContextMenuClick = (
+    dataSourceName: string,
+    columnKey: string,
+    fieldName: string,
+    contextValue: string,
+    contextLabel: string
+  ) => {
+    console.log(contextValue);
+  };
+
+  private _onContextMenuRender = (
+    dataSourceName: string,
+    columnKey: string,
+    fieldName: string,
+    contextValue: string,
+    contextLabel: string
+  ) => {
+    return <span>{contextLabel}</span>;
+  };
+  
+
+  public render() {
+    return (
+        <div>
+       
+        {this.state.dataSource.length > 0 && (
+          <TVoSimpleGrid
+            dataSource={this.state.dataSource}
+            columnDefs={this.state.columns}
+            onScrollToBottom={this._onScroll}
+            onRowItemRender={this._onRowItemRender}
+            onHeaderItemRender={this._onHeaderItemRender}
+            onContextMenuClick={this._onContextMenuClick}
+            onContextMenuRender={this._onContextMenuRender}
+            onRowItemClick={this._onRowItemClick}
+            disableGridScroll={false}
+          />
+        )}
+      </div>
+    );
+  }
+}
---
+++ src/data/ColumnDefinition.ts
@@ -0,0 +1,120 @@
+import { IColumnDefs } from "tvo-react-simple-grid";
+
+export const ColumnDefinition: IColumnDefs = {
+    dataSourceName:'PersonalInfo',
+    columns:[
+        {
+            headerName: 'ID',
+            key: 'ID',
+            fieldName: 'ID',
+            enableOnHeaderItemRender:true,  
+            enableOnRowItemClick:false,
+            enableOnRowItemRender:true,
+            enableContextMenu:true
+           
+        },
+        {
+            headerName: 'First Name',
+            key: 'FirstName',
+            fieldName: 'FirstName',
+            enableOnHeaderItemRender:true,  
+            enableOnRowItemClick:false,
+            enableOnRowItemRender:true,
+            enableContextMenu:true,
+            contextMenu:[
+                {
+                    value:'Item1',
+                    label:'Item 1',
+                    enableOnContextMenuRender:true
+                },
+                {
+                    value:'Item2',
+                    label:'Item 2',
+                    enableOnContextMenuRender:true,
+                    enableBottomDivider:true
+                },
+                {
+                    value:'Item3',
+                    label:'Item 3',
+                    enableOnContextMenuRender:true
+                },
+                {
+                    value:'Item4',
+                    label:'Item 4',
+                    enableOnContextMenuRender:true
+                }
+            ]
+        },
+        {
+            headerName: 'Last Name',
+            key: 'LastName',
+            fieldName: 'LastName',
+            enableOnHeaderItemRender:true,  
+            enableOnRowItemClick:true,
+            enableOnRowItemRender:true,
+            enableContextMenu:true,
+            contextMenu:[
+                {
+                    value:'Item5',
+                    label:'Item 5',
+                    enableOnContextMenuRender:true
+                },
+                {
+                    value:'Item6',
+                    label:'Item 6',
+                    enableOnContextMenuRender:true,
+                    enableBottomDivider:true
+                }
+            ]
+        },
+        
+        {
+            headerName: 'Job Title',
+            key: 'JobTitle',
+            fieldName: 'JobTitle',
+            enableOnHeaderItemRender:true,  
+            enableOnRowItemClick:true,
+            enableOnRowItemRender:true,
+            enableContextMenu:true,
+            contextMenu:[
+                {
+                    value:'Item8',
+                    label:'Item 8',
+                    enableOnContextMenuRender:true
+                }
+            ]
+        },
+        {
+            headerName: 'Email ',
+            key: 'Email',
+            fieldName: 'EmailAddress',
+            enableOnHeaderItemRender:true,  
+            enableOnRowItemClick:true,
+            enableOnRowItemRender:true,
+            enableContextMenu:true,
+            contextMenu:[
+                {
+                    value:'Item9',
+                    label:'Item 9',
+                    enableOnContextMenuRender:true
+                },
+                {
+                    value:'Item10',
+                    label:'Item 10',
+                    enableOnContextMenuRender:true
+                },
+                {
+                    value:'Item11',
+                    label:'Item 11',
+                    enableOnContextMenuRender:true,
+                    enableBottomDivider:true
+                },
+                {
+                    value:'Item12',
+                    label:'Item 12',
+                    enableOnContextMenuRender:true
+                }
+            ]
+        }
+    ]
+}
